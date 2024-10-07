1cee9f82170e32650a10eec8095d817d78086c89
fzybot <ruslan.akhpashev@gmail.com>
2024-10-07 07:36:49 +0300

    Основной каркас (backbone) для получения данных о местоположении устройства. Обработка запроса разрешений.

--- app/src/main/java/com/example/cellularanalyzer/MainActivity.kt
+++ app/src/main/java/com/example/cellularanalyzer/MainActivity.kt
@@ -1,29 +1,140 @@
 package com.example.cellularanalyzer
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
+const val PERMISSION_REQUEST_CAMERA = 0
+
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
 
     val value: Int = 0
     val LOG_TAG: String = "myLogs"
 
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION= 100
+    }
+
+    private lateinit var myFusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvLat: TextView
+    private lateinit var tvLon: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(LOG_TAG, "onCreate");
+        Log.d(LOG_TAG, "onCreate")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
         val helloWorldView: TextView  = findViewById(R.id.helloWorldTextView) as TextView
         helloWorldView.text = "onCreate hello"
         Log.d(LOG_TAG, helloWorldView.text.toString());
+
+
+
+        myFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        tvLat = findViewById(R.id.lat) as TextView
+        tvLon = findViewById(R.id.lon) as TextView
+
+        getCurrentLocation()
+
+
+    }
+
+    private fun getCurrentLocation(){
+
+        if(checkPermissions()){
+            if(isLocationEnabled()){
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions()
+                    return
+                }
+                myFusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+                    val location: Location?=task.result
+                    if(location == null){
+                        Toast.makeText(applicationContext, "problems with signal", Toast.LENGTH_SHORT).show()
+                    } else {
+                        tvLat.setText(location.latitude.toString())
+                        tvLon.setText(location.longitude.toString())
+                    }
+                }
+
+            } else{
+                // open settings to enable location
+                Toast.makeText(applicationContext, "Enable location in settings", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            Log.w(LOG_TAG, "location permission is not allowed");
+            tvLat.setText("Permission is not granted")
+            tvLon.setText("Permission is not granted")
+            requestPermissions()
+        }
+
+    }
+
+    private fun requestPermissions() {
+        Log.w(LOG_TAG, "requestPermissions()");
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    private fun checkPermissions(): Boolean{
+        if( ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED )
+        {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSION_REQUEST_ACCESS_LOCATION)
+        {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(applicationContext, "Permission granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            } else {
+                Toast.makeText(applicationContext, "Denied by user", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        val locationManager:LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
     }
 
 
@@ -34,4 +145,6 @@ class MainActivity : AppCompatActivity() {
         helloWorldView.setText("on Resume Hello")
         Log.d(LOG_TAG, helloWorldView.text.toString());
     }
+
+    
 }
